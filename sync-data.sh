feca7d51d65314b0d1ef782e481fb3a909c9f2f5
Ovidiu Miron <omiron1@yahoo.com>
2014-05-13 18:33:50 +0300

    added synchronisation script 1. stop all instance 2. remove filestorage folder 3. copy from production filestorage folder to staging 4. remove datafs folder 5. copy from production datafs folder to staging 6. start all instance

---
+++ sync-data.sh
@@ -0,0 +1,51 @@
+#-----------------GLOBAL VARIABLES 
+PRODUCTION_DIR=""
+STAGING_DIR=""
+#----------------------------
+
+#----------------GENERAL FUNCTIONS
+log() {
+    echo "$(date)-->$1"
+}
+
+remove_dir() {
+    if [ -d "$1" ]; then
+        rm -rf "$1"
+        if [ "$?" -ne "0" ]; then
+            log "Can't remove $1 folder"
+            exit 1
+        fi
+    fi
+}
+#----------------MAIN 
+#Assumed that exists filestorage and blobstorage folders
+
+log "Starting sync script"
+
+$STAGING_DIR/bin/supervisorctl stop all
+if [ "$?" -ne "0" ]; then
+    log "Can't stop all from supervisorctl"
+    exit 1
+fi
+
+remove_dir "$STAGING_DIR/var/filestorage"
+cp -r $PRODUCTION_DIR/var/filestorage $STAGING_DIR/var/
+if [ "$?" -ne "0" ]; then
+    echo "Can't copy filestorage"
+    exit 1
+fi
+
+remove_dir "$STAGING_DIR/var/blobstorage"
+cp -r $PRODUCTION_DIR/var/blobstorage $STAGING_DIR/var/
+if [ "$?" -ne "0" ]; then
+    echo "Can't copy blobstorage"
+    exit 1
+fi
+
+$STAGING_DIR/bin/supervisorctl start all
+if [ "$?" -ne "0" ]; then
+    log "Can't start all from supervisorctl"
+    exit 1
+fi
+
+log "Synchronisation done with success"
